8631465a3457d2f9e124dccf716771c3d1792972
Dharanikamaraj15 <k.dharanisree@gmail.com>
2026-04-29 19:00:15 +0530

    Lists exercises

---
+++ Tuple.py
@@ -0,0 +1,32 @@
+# fruits = ("apple", "banana", "cherry",1,123) - CIRCLE BRACETS
+# print(fruits)
+
+#Lists: Ordered
+# names= ("Aadhya", "Bala", "Candy")
+# names1 =("A","B","C")
+# print(names[0])
+# print(names)
+# print(names1)
+
+#Lists: Mutable -Error- Not woring
+# names= ("Aadhya", "Bala", "Candy")
+# names1 =("A","B","C")
+# print(names)
+# print(names1)
+# names[2]="Dharani"
+# print(names)
+
+#Lists: Duplicates
+# names= ("Aadhya", "Bala", "Candy", "Aadhya", "aadhya")
+# print(names)
+
+#Lists: Indexing
+# names= ("Aadhya", "Bala", "Candy")
+# print(names)
+# print(names[0])
+# print(names[-1])
+# print(names[2])
+
+#Lists: eyvalue pair
+# names= ["name": "Aadhya", "Age:" "35"]
+# --Not woring
