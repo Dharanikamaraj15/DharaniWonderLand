129c1e2cf1d2d53c4f09e90bd0460cbc99bd685f
Dharanikamaraj15 <k.dharanisree@gmail.com>
2026-05-05 18:13:40 +0530

    String exercises

--- Functions.py
+++ Functions.py
@@ -30,9 +30,15 @@
 #     print("Hello", name)
 # greeting("harani")
 
-#Recursive function- A Function that calls itself again and again
-def recursive(n):
-   if n == 1:
-       return 1
-   return n * recursive(n-1)
-print(recursive(5))
+# #Recursive function- A Function that calls itself again and again
+# def recursive(n):
+#    if n == 1:
+#        return 1
+#    return n * recursive(n-1)
+# print(recursive(5))
+
+# def Hello():
+#     print("Hello class")
+# Hello()
+
+
---
+++ Strings.py
@@ -0,0 +1,23 @@
+text= "Hello Dharani"
+age=35
+# name=text.lower()
+# name=text.strip()
+# name = text.replace("Dharani", "Aadhya")
+# name=text.split(" ") # Converts into list
+# name=text.find("D")
+# name=text.count("l")
+# name=text[0:5] #slicing
+# name=text[::-1] #Reverse
+# print(name)
+# print("HELLO".lower())
+# print("hello".upper())
+# print(f"I'm {text} and I'm {age} years old") #using f function
+# print("Hello \n World")
+# print("Hello \t World")
+
+#String Slicing
+# text = "Dharani"
+# print(text[0:4])   # Pyth
+# print(text[::-1])  # Reverse
+
+
