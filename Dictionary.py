7b2b837293046d0137220844d033177946abf0cf
Dharanikamaraj15 <k.dharanisree@gmail.com>
2026-05-06 19:03:02 +0530

    numpy learnings

--- Dictionary.py
+++ Dictionary.py
@@ -26,7 +26,7 @@
 # fruits={ "name" :"Apple", "color" : "red", "plan" : "123"}
 # print(fruits[0])
 
-#Dictionary: eyvalue pair
-# names= ["name: " "Aadhya", "Age:" "35"]
-# print(names)
+# Dictionary: eyvalue pair
+names= ["name: " "Aadhya", "Age:" "35"]
+print(names)
 
---
+++ Numpy Learnings.py
@@ -0,0 +1,86 @@
+import numpy as np
+# a=np.array([1,2])
+# print(a)
+# b = np.array([[1, 2],[3,4]])
+# print(b)
+# c=np.zeros((2,3))
+# print(c)
+# d=np.ones((3,3)
+# print(d)
+# e=np.eye(3) prints diagonally
+# print(e)
+# f=np.arange(0,10,2) -- This prints 0 to 10 with the range of 2
+# print(f)
+# g=np.linspace(0,1,5) -- this creates 5 values between 0 and 1
+# print(g)
+# d=np.ones((3,3))
+# print(d)
+# print(d.shape)
+# print(d.dtype)
+# a = np.array([10,20,30,40]) #Single dimension
+# print(a[0]) 
+# print(a[1:3]) 
+# a=np.array([[1,2,3],
+#             [4,5,6]])
+# print(a)
+# print(a[1,1])
+# print(a[:,1])   # prints first column  as rows
+# a = np.array([1,2,3])
+# b = a[0:2]
+# print(a)
+# print(b)
+# b[0]=100
+# print(b)
+# a=np.array([1,2,3])
+# b=np.array([4,5,6])
+# a = a+ 10 #scalar operations 
+# b= b * 2 #scalar operations 
+# print(a)
+# print(b)
+# c=a+b
+# d=a-b
+# e=a*b
+# print(c)
+# print(d)
+# print(e)
+# a = np.array([1,2,3])
+# b = 10
+# c= a + b
+# print(c)
+# a = np.array([[1,2,3],
+#              [4,5,6]])
+# b = np.array([1,1,1])
+# c= a + b
+# print(c)
+# c = np.array([1,2,3])
+# b= np.array([[1,2,3],[4,5,6]])
+# a=c+b
+# print(a)
+# print(np.sum(a))
+# print(np.mean(a))
+# print(np.max(a))
+# print(np.min(a))
+
+# a = np.arange(6)
+# print(a)
+# b= a.reshape(2,3)
+# print (b)
+
+# a = np.array([[1,2,3],
+#              [4,5,6]])
+# c= a.flatten()
+# print (c)
+
+# marks = np.array([80, 90, 70, 60, 85])
+# a= np.mean(marks)
+# b= np.max(marks)
+# c= marks > 70
+# print(a)
+# print(b)
+# print(c)
+
+
+
+
+
+
