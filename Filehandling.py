1f6f7cd65c80e07dc17abe1e24483aa12b144a16
Dharanikamaraj15 <k.dharanisree@gmail.com>
2026-05-07 18:47:55 +0530

    File handling exercises

---
+++ Filehandling.py
@@ -0,0 +1,31 @@
+# file = open("data.txt", "w")
+# file.write("Hello World")
+# file.close()
+
+# file = open("data.txt", "r")
+# print(file.read())
+# content= file.read()
+# print(content)
+
+# file = open("data.txt", "r")
+# print(file.readline())
+# content= file.read()
+# print(content)
+
+# file = open("data.txt", "r")
+# print(file.readlines())
+
+# file= open("Datatry.txt","w")
+# file.write("Helllow Dharani, Enjoy your learnings")
+# file.close()
+
+# file= open("Datatry.txt","a")
+# file.write("\n Continue your learning")
+# file.close()
+
+# file = open("data.txt", "r")
+# print(file.read())
+# file.close()
+
+# with open("data.txt","r") as file:
+#     print(file.read())
